399f758f9606772be2e19a7c68e16a9adcefe50f
RonhoGroniko <sarapovrodion@gmail.com>
2025-09-17 21:50:54 +0200

    Add DateFormatter.kt

--- app/src/main/java/com/sharapov/notes/presentation/screens/notes/NotesScreen.kt
+++ app/src/main/java/com/sharapov/notes/presentation/screens/notes/NotesScreen.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.itemsIndexed
@@ -29,12 +30,14 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.sharapov.notes.domain.Note
 import com.sharapov.notes.presentation.ui.theme.OtherNotesColors
 import com.sharapov.notes.presentation.ui.theme.PinnedNotesColors
+import com.sharapov.notes.presentation.utils.DateFormatter
 
 @Composable
 fun NotesScreen(
@@ -89,6 +92,7 @@ fun NotesScreen(
                     key = { _, note -> note.id }
                 ) { index, note ->
                     NoteCard(
+                        modifier = Modifier.widthIn(max = 160.dp),
                         note = note,
                         background = PinnedNotesColors[index % PinnedNotesColors.size],
                         onNoteClick = {
@@ -240,11 +244,13 @@ fun NoteCard(
         Text(
             text = note.title,
             fontSize = 14.sp,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
             color = MaterialTheme.colorScheme.onSurface
         )
         Spacer(modifier = Modifier.height(8.dp))
         Text(
-            text = note.updatedAt.toString(),
+            text = DateFormatter.formatDateToString(note.updatedAt),
             fontSize = 12.sp,
             color = MaterialTheme.colorScheme.onSurfaceVariant
         )
@@ -252,6 +258,8 @@ fun NoteCard(
         Text(
             text = note.content,
             fontSize = 16.sp,
+            maxLines = 3,
+            overflow = TextOverflow.Ellipsis,
             color = MaterialTheme.colorScheme.onSurface,
             fontWeight = FontWeight.Medium
         )
---
+++ app/src/main/java/com/sharapov/notes/presentation/utils/DateFormatter.kt
@@ -0,0 +1,27 @@
+package com.sharapov.notes.presentation.utils
+
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.util.concurrent.TimeUnit
+
+object DateFormatter {
+
+    private val millisInHour = TimeUnit.HOURS.toMillis(1)
+    private val millisInDay = TimeUnit.DAYS.toMillis(1)
+    private val formatter = SimpleDateFormat.getDateInstance(DateFormat.SHORT)
+
+    fun formatDateToString(timeStamp: Long): String {
+        val now = System.currentTimeMillis()
+        val diff = now - timeStamp
+        return when {
+            diff < millisInHour -> "Just now"
+            diff < millisInDay -> {
+                val hours = TimeUnit.MILLISECONDS.toHours(diff)
+                "$hours h ago"
+            }
+            else -> {
+                formatter.format(timeStamp)
+            }
+        }
+    }
+}
