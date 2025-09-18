b8545b0eecf54680750fd6e59823a22b4c457589
RonhoGroniko <sarapovrodion@gmail.com>
2025-09-18 15:21:51 +0200

    Add Jetpack navigation

--- app/src/main/java/com/sharapov/notes/presentation/MainActivity.kt
+++ app/src/main/java/com/sharapov/notes/presentation/MainActivity.kt
@@ -1,12 +1,10 @@
 package com.sharapov.notes.presentation
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.sharapov.notes.presentation.navigation.NavGraph
-import com.sharapov.notes.presentation.screens.editing.EditNoteScreen
 import com.sharapov.notes.presentation.ui.theme.NotesTheme
 
 class MainActivity : ComponentActivity() {
--- app/src/main/java/com/sharapov/notes/presentation/navigation/NavGraph.kt
+++ app/src/main/java/com/sharapov/notes/presentation/navigation/NavGraph.kt
@@ -1,41 +1,45 @@
 package com.sharapov.notes.presentation.navigation
 
 
+import android.os.Bundle
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.sharapov.notes.presentation.screens.creation.CreateNoteScreen
 import com.sharapov.notes.presentation.screens.editing.EditNoteScreen
 import com.sharapov.notes.presentation.screens.notes.NotesScreen
 
 @Composable
 fun NavGraph() {
-    val screen = remember {
-        mutableStateOf<Screen>(Screen.Notes)
-    }
-    when(val currentState = screen.value) {
-        Screen.CreateNote -> {
-            CreateNoteScreen(
-                onFinished = {
-                    screen.value = Screen.Notes
-                }
-            )
-        }
-        is Screen.EditNote -> {
-            EditNoteScreen(
-                noteId = currentState.id,
-                onFinished = {
-                    screen.value = Screen.Notes
-                }
-            )
-        }
-        Screen.Notes -> {
+    val navController = rememberNavController()
+    NavHost(
+        navController = navController,
+        startDestination = Screen.Notes.route
+    ) {
+        composable(Screen.Notes.route) {
             NotesScreen(
                 onFABClick = {
-                    screen.value = Screen.CreateNote
+                    navController.navigate(Screen.CreateNote.route)
                 },
                 onNoteClick = {
-                    screen.value = Screen.EditNote(it.id)
+                    navController.navigate(Screen.EditNote.createRoute(it.id))
+                }
+            )
+        }
+        composable(Screen.CreateNote.route) {
+            CreateNoteScreen(
+                onFinished = {
+                    navController.popBackStack()
+                }
+            )
+        }
+        composable(Screen.EditNote.route) {
+            val noteId = Screen.EditNote.getNoteId(it.arguments)
+            EditNoteScreen(
+                noteId = noteId,
+                onFinished = {
+                    navController.popBackStack()
                 }
             )
         }
@@ -43,9 +47,18 @@ fun NavGraph() {
 }
 
 
-sealed interface Screen {
+sealed class Screen(val route: String) {
 
-    data object Notes: Screen
-    data object CreateNote: Screen
-    data class EditNote(val id: Int): Screen
+    data object Notes : Screen("notes")
+    data object CreateNote : Screen("create_note")
+    data object EditNote : Screen("edit_note/{note_id}") {
+
+        fun createRoute(noteId: Int): String {
+            return "edit_note/$noteId"
+        }
+
+        fun getNoteId(arguments: Bundle?): Int {
+            return arguments?.getString("note_id")?.toInt() ?: 0
+        }
+    }
 }
