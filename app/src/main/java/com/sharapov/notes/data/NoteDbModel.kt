b4262d79200f32bdc3ea6d841ecbefe789358926
RonhoGroniko <sarapovrodion@gmail.com>
2025-09-18 16:49:22 +0200

    Add NotesDao.kt

--- app/src/main/java/com/sharapov/notes/data/NoteDbModel.kt
+++ app/src/main/java/com/sharapov/notes/data/NoteDbModel.kt
@@ -4,7 +4,7 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 
-@Entity
+@Entity(tableName = "notes")
 data class NoteDbModel(
     @PrimaryKey(autoGenerate = true)
     val id: Int,
